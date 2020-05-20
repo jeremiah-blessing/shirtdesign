6489ea327d5b786a043f346d31fd506389f21a0b
Jeremiah Blessing <vjeremiahblessing@gmail.com>
2020-05-20 20:21:33 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/shirtdesign/precache-manifest.26f0bd9f0ca93afa0fe0f665fff12005.js"
+  "/shirtdesign/precache-manifest.8e2a82e9118d673a985f413603c2c7c7.js"
 );
 
 self.addEventListener('message', (event) => {
